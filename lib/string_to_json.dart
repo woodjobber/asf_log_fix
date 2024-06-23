52bbfe763848980da9567202f492da1832d490be
soer <474436731@qq.com>
2024-06-23 14:38:58 +0800

    [INFO]:json容错处理

--- lib/string_to_json.dart
+++ lib/string_to_json.dart
@@ -22,10 +22,33 @@ extension StringToJson on String {
           value = value.substring(0, value.length - 1);
         }
         if (key.startsWith('{')) {
-          key = key.substring(1, key.length - 1);
+          key = key.substring(1);
         }
+        key = key.trim().replaceAll(RegExp(r'\"|\x20'), '');
+
         // Assign key-value pair to the map
-        jsonMap[key] = value;
+        String pattern = r'^[0-9]+$';
+        bool hasMatch = RegExp(pattern).hasMatch(value ?? '');
+        String pattern2 = r'^[+-]?(\d*\.\d+|\d+\.\d*)$';
+        bool hasMatch2 = RegExp(pattern2).hasMatch(value ?? '');
+        if (hasMatch || hasMatch2) {
+          if (hasMatch2) {
+            RegExp trailingZeros = RegExp(r'^[0-9]+.0+$');
+            if (trailingZeros.hasMatch(value!)) {
+              jsonMap[key] = double.parse(value).ceilToDouble();
+            } else {
+              jsonMap[key] = double.tryParse(value);
+            }
+          } else {
+            jsonMap[key] = int.tryParse(value ?? '');
+          }
+        } else if (value == 'true' || value == 'false') {
+          jsonMap[key] = value == 'true';
+        } else if (value == 'null') {
+          jsonMap[key] = null;
+        } else {
+          jsonMap[key] = value?.replaceAll(RegExp(r'^[/"]|[/"]$'), '');
+        }
       }
     }
     return jsonMap;
