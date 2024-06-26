0526f83c7365bf21c5016ecccc0abb53bef667b5
soer <474436731@qq.com>
2024-06-26 21:16:26 +0800

    [INFO]:增加智能修复匹配规则

--- lib/beauty_json.dart
+++ lib/beauty_json.dart
@@ -109,7 +109,7 @@ class _BeautyJsonPageState extends State<BeautyJsonPage> {
                           // Pattern regex = RegExp(
                           //     r'\[.{0,16}\]\s?I?/?flutter(?:.*?):\s?|I?/?flutter(?:[^\[:\]]*?):\s?');
                           RegExp regex = RegExp(
-                              r'\[.{0,16}\]\s?I?/?flutter.*?:\s?|I?/?flutter[^:\[\]]*:\s?|.{0,25}\s?I/flutter[.*]{0,12}:\s? | .{0,25}\s?flutter[.*]{0,12}:\s?');
+                              r'\[.{0,16}\]\s?I?/?flutter.*?:\s?|I?/?flutter[^:\[\]]*:\s?|.{0,25}\s?I/flutter.*?:\s? | .{0,25}\s?flutter.*?:\s?');
                           var oldText = controller.text;
                           var jsonText = oldText.replaceAll(regex, '');
                           if (regex.hasMatch(oldText)) {
